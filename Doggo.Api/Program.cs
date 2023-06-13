4690f5676462e41600ef267151bbb366a7b153b4
Nazar Turchynovych <nazariiturchynovych@gmail.com>
2023-06-13 18:35:18 +0300

    Added SeriLog

--- Doggo.Api/Program.cs
+++ Doggo.Api/Program.cs
@@ -4,64 +4,82 @@ using Doggo.Extensions;
 using Doggo.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File(
+        path: "/Users/turchynovychnazarii/Desktop/LogPath/log-.txt",
+        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{level:u3}] {Message:lj}{NewLine}{Exception}",
+        rollingInterval: RollingInterval.Day,
+        restrictedToMinimumLevel: LogEventLevel.Information)
+    .CreateBootstrapLogger();
 
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGenWithJwt();
-
-builder.Services.AddDbContext<DoggoDbContext>( options =>
+try
 {
-    options.UseNpgsql(builder.Configuration.GetConnectionString(ConnectionConstants.Postgres));
-});
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services
-    .AddIdentity<User, Role>()
-    .AddUserManager<UserManager<User>>()
-    .AddEntityFrameworkStores<DoggoDbContext>()
-    .AddDefaultTokenProviders();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGenWithJwt();
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: "MyAllowSpecificOrigins",
-        policy  =>
+    builder.Services.AddDbContext<DoggoDbContext>(
+        options =>
         {
-            policy.AllowAnyMethod();
-            policy.AllowAnyHeader();
-            policy.AllowAnyOrigin();
+            options.UseNpgsql(builder.Configuration.GetConnectionString(ConnectionConstants.Postgres));
         });
-});
 
+    builder.Services
+        .AddIdentity<User, Role>()
+        .AddUserManager<UserManager<User>>()
+        .AddEntityFrameworkStores<DoggoDbContext>()
+        .AddDefaultTokenProviders();
 
-builder.ConfigureIdentity();
-builder.RegisterAuthentication();
-builder.RegisterOptions();
-builder.RegisterServices();
-builder.RegisterRepositories();
+    builder.Services.AddCors(
+        options =>
+        {
+            options.AddPolicy(
+                name: "MyAllowSpecificOrigins",
+                policy =>
+                {
+                    policy.AllowAnyMethod();
+                    policy.AllowAnyHeader();
+                    policy.AllowAnyOrigin();
+                });
+        });
 
+    builder.ConfigureIdentity();
+    builder.RegisterAuthentication();
+    builder.RegisterOptions();
+    builder.RegisterServices();
+    builder.RegisterRepositories();
 
-var app = builder.Build();
+    var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-app.UseCors("MyAllowSpecificOrigins");
+    app.UseCors("MyAllowSpecificOrigins");
 
-app.UseAuthentication();
-app.UseAuthorization();
+    app.UseAuthentication();
+    app.UseAuthorization();
 
-app.UseHttpsRedirection();
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-app.MapControllers();
+    app.UseHttpsRedirection();
+    app.MapControllers();
 
 // app.SeedUsersAndRolesAsync().Wait();
 
-app.Run();
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled Exception");
+}
+finally
+{
+    Log.Information("shut down complete");
+    Log.CloseAndFlush();
+}
