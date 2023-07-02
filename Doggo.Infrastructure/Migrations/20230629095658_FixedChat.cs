eb5185bd61e21f10c4fd4a262a2dac35679a7d36
Nazar Turchynovych <nazariiturchynovych@gmail.com>
2023-07-02 12:26:04 +0300

    fix

---
+++ Doggo.Infrastructure/Migrations/20230629095658_FixedChat.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Doggo.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedChat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsOneToOne",
+                table: "Chats",
+                newName: "IsPrivate");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ChangedDate",
+                table: "Messages",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Messages",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChangedDate",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Messages");
+
+            migrationBuilder.RenameColumn(
+                name: "IsPrivate",
+                table: "Chats",
+                newName: "IsOneToOne");
+        }
+    }
+}
